d7a51c92c9f5ab27f67fb4a6c148a00b65cfa6d6
TPhuocDat <dattp1202@gmail.com>
2024-12-25 16:09:56 +0700

    test thu xem
    test

--- src/testcode.java
+++ src/testcode.java
@@ -2,4 +2,7 @@ public class testcode {
     public static void main(String[] args) {
         System.out.println("eeeee, World!");
     }
+    public static void ggaaa(String[] args) {
+        System.out.println("eeeee, World!");
+    }
 }
