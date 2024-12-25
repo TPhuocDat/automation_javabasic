f663d51779567150d1fde065f9675b49613b49a2
TPhuocDat <dattp1202@gmail.com>
2024-12-25 16:18:30 +0700

    Update testcode.java

--- src/testcode.java
+++ src/testcode.java
@@ -2,7 +2,7 @@ public class testcode {
     public static void main(String[] args) {
         System.out.println("eeeee, World!");
     }
-    public static void ggaaa(String[] args) {
-        System.out.println("eeeee, World!");
+    public static void aaaaa (String[] args) {
+        System.out.println("eeeee, baaaaaaa!");
     }
 }
