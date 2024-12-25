91b91178a4d4ee1490dc22874d8e73513143d0ae
TPhuocDat <dattp1202@gmail.com>
2024-12-25 16:36:14 +0700

    Update testcode.java

--- src/testcode.java
+++ src/testcode.java
@@ -3,6 +3,6 @@ public class testcode {
         System.out.println("eeeee, World!");
     }
     public static void aaaaa (String[] args) {
-        System.out.println("eeeee, baaaaaaa!");
+        System.out.println("eeeee, gggggaaaaaweeee!");
     }
 }
